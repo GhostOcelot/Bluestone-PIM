d7a8ad8eb080872efe5f7db26f96946058d72914
GhostOcelot <houseplayer@gmail.com>
2025-06-08 17:59:52 +0200

    fix another prettier error

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
 import react from "eslint-plugin-react"
 import jsxA11y from "eslint-plugin-jsx-a11y"
-import prettierPlugin from "eslint-plugin-prettier";
+import prettierPlugin from "eslint-plugin-prettier"
 import prettierConfig from "eslint-config-prettier"
 
 export default tseslint.config(
